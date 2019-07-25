1cb33a17808a1feda439e78ff5766951b8f9a93d
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-07-25 11:26:46 -0700

    Sandbox lists

--- lists.py
+++ lists.py
@@ -1,1 +1,46 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+
+# List definition
+# numbers = [1, 2, 3, 4, 5]
+
+# Using a constructor
+numbers = list((1, 2, 3, 4, 5))
+
+print(numbers)
+print(type(numbers))
+
+fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
+
+# Select array index
+print(fruits[2])
+
+# Length
+print(len(fruits))
+
+# Append to list
+fruits.append('Mangos')
+print(fruits)
+
+# Remove from list
+fruits.remove('Grapes')
+print(fruits)
+
+# Insert into position
+fruits.insert(2, 'Strawberries')
+print(fruits)
+
+# Remove from position
+fruits.pop(3)
+print(fruits)
+
+# Reverse list
+fruits.reverse()
+print(fruits)
+
+# Sort list (Default alphabetic)
+fruits.sort()
+print(fruits)
+
+# Reverse sort list
+fruits.sort(reverse=True)
+print(fruits)
