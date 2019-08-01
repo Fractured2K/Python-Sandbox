6a2b7ee79929b90f29a65112968f3ae4f86af3bd
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-08-01 12:00:19 -0700

    Sets

--- tuples_sets.py
+++ tuples_sets.py
@@ -25,3 +25,25 @@ del fruit_tuple_trailing
 print(len(fruit_tuple))
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+fruit_set = {'Apple', "Orange", 'Mango', 'Apple'}
+print(fruit_set)
+
+# Check if in set
+print('Apple' in fruit_set)  # True
+print('Apples' in fruit_set)  # False
+
+# Add to set
+fruit_set.add('Grape')
+print(fruit_set)
+
+# Remove from set
+fruit_set.remove('Grape')
+print(fruit_set)
+
+# Clear set
+fruit_set.clear()
+print(fruit_set)
+
+# Delete set
+# del fruit_set
+# print(fruit_set)
